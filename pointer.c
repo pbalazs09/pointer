b91f28cf06e2ffe05773996be71209977d5bc68f
Balázs Pálfi <pbalazs09@gmail.com>
2018-10-20 20:07:38 +0200

    Initial commit

---
+++ pointer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+double szamok1[5] = { 4.5, 9.2, 7.1, -6.9, 8 };
+double szamok2[5] = { 9.3, 78, -7, 0.01, 4.6 };
+
+double* p;  /* pointer típusú változó */
+
+p = &szamok1[1];
+printf("%f\n", *p);  /* 9.2 */
+
+p = &szamok2[3];
+*p = 5.7;            /* a szamok2[3]-at */
+
+scanf("%lf", &szamok1[2]);
+
+return 0;
+}
