24102bd49e4fa74b062a59550cc72f96299c768c
Balázs Pálfi <pbalazs09@gmail.com>
2018-10-20 20:09:50 +0200

    Update pointer.c
    Initial commit

--- pointer.c
+++ pointer.c
@@ -4,7 +4,7 @@ int main(void){
 double szamok1[5] = { 4.5, 9.2, 7.1, -6.9, 8 };
 double szamok2[5] = { 9.3, 78, -7, 0.01, 4.6 };
 
-double* p;  /* pointer típusú változó */
+double* p;  /* pointer tï¿½pusï¿½ vï¿½ltozï¿½ */
 
 p = &szamok1[1];
 printf("%f\n", *p);  /* 9.2 */
